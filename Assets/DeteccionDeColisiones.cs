fe4b8c6f7cee7a290e090e4657aa606f108b7c47
joaquinmora14 <166827330+joaquinmora14@users.noreply.github.com>
2025-05-23 18:47:47 -0300

    PROBLEMA COLLISIONS

---
+++ Assets/DeteccionDeColisiones.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision collision)
+   {
+    Debug.Log("Contacto");
+    Destroy(collision.gameObject);
+
+   }
+}
